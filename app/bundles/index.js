2439835bd7209dcc768fa28e33bd1cd7f9207a1f
Marenqing <1213642578@qq.com>
2016-08-25 14:23:13 +0800

    first commit.

---
+++ app/bundles/index.js
@@ -0,0 +1,3 @@
+var hello = require('./export');
+
+console.log(hello());
---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+//定义类
+class Index {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return '(' + this.x + ', ' + this.y + ')';
+  }
+}
+
